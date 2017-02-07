81765ef6f6458d5f8c7df4b06eddec9918e633dd
Peter MOUEZA <peter.poe11@formation.imie>
2017-02-07 09:28:40 +0100

    sov

--- JS/js2gestionnaireContacts.js
+++ JS/js2gestionnaireContacts.js
@@ -6,19 +6,20 @@
 		console.log("idString="+idString);
 		var memContactIndex;
 		var c = new Contact();
+		var trouve =false;
 		i=0;
 		while((!trouve)&&(i<tabloContact.length)){
-          if (idString==(tabloContact[i].id){
+          if (idString==(tabloContact[i].id)){
 			memContactIndex=i;
           }else{
           	console.log('not found i='+i);
           }
           i++;
 		}
-	c.id=tabloContact[memContactIndex].id;
-	c.prenom=tabloContact[memContactIndex].prenom;
-	c.nom=tabloContact[memContactIndex].nom;
-	return c;
+		c.id=tabloContact[memContactIndex].id;
+		c.prenom=tabloContact[memContactIndex].prenom;
+		c.nom=tabloContact[memContactIndex].nom;
+		return c;
 	}
 
 $(document).ready(function(){
@@ -76,77 +77,34 @@ $(document).ready(function(){
 
 
 
-	$("#valider").click(function(){
-			console.log("valider");
-			jeu.trippedpropositionCourante=[];
-			for(var caseOf4=0;caseOf4<4;caseOf4++){
-					jeu.trippedpropositionCourante[caseOf4]=jeu.propositionCourante[jeu.propositionCourante.length-1-3];
-			}
-			console.log("jeu.propositionCourante="+jeu.propositionCourante);
-			console.log("trippedpropositionCourante="+jeu.trippedpropositionCourante);
-			table5454+=	'<tr><td>'+jeu.trippedpropositionCourante[0]+'</td><td>'+jeu.trippedpropositionCourante[1]+'</td><td>'+jeu.trippedpropositionCourante[2]+'</td><td>'+jeu.trippedpropositionCourante[3]+'</td></tr>';   
-			console.log('table5454='+table5454);
-			$("#grosTablo").html(table5454);	
-
-			var l1=new Ligne(jeu.propositionCourante);
-			var l2=new Ligne(jeu.ligneAlea);
-
-			res=jeu.comparer2lignes(l1,l2);
-			console.log("res.length="+res.length);
-			console.log("res.indices="+res.indices);
-			
 
 
-		    jeu.propositionCourante=[];
-		    trippedpropositionCourante=[];
-		    jeu.size=0;
-								   }
-	);
-
-	
-
-
-
-
-	$("#effacer").click(function(){
-	console.log("effacer");
-	jeu.propositionCourante=[];
-								   }
-	);
-
-	
 
 
 	$("#pionvert").click(function(){
 		console.log("pionvert");
-		jeu.propositionCourante[jeu.propositionCourante.length]=jeu.couleur[0];
-								   }
+									   }
 	);
 
 	$("#pionbleu").click(function(){
 		console.log("pionbleu");
-		jeu.propositionCourante[jeu.propositionCourante.length]=jeu.couleur[2];
-								   }
+										   }
 	);
 
 	$("#pionjaune").click(function(){
 		console.log("pionjaune");
-		jeu.propositionCourante[jeu.propositionCourante.length]=jeu.couleur[1];
-								   }
+										   }
 	);
 
 	$("#pionrouge").click(function(){
 		console.log("pionrouge");
-		jeu.propositionCourante[jeu.propositionCourante.length]=jeu.couleur[3];
-								   }
+										   }
 	);
 
 
 	$(".pionCouleur").click(function(){
 		console.log("pion couleur.  ");
-		//jeu.propositionCourante[jeu.propositionCourante.length]=jeu.couleur[0];
-		jeu.afficherCourant();
-								   }
+									   }
 	);
 
 
---
+++ JS/js2gestionnaireContactsRes.js
@@ -0,0 +1,16 @@
+function Res(){
+	this.id="";	
+	this.nom="";
+	this.prenom="";
+	
+
+
+
+	this.setContact=function(contact){
+		this.nom=contact.nom;
+		this.prenom=contact.prenom;
+	}
+
+}
+
+
