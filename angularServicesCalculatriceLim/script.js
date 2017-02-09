fccd336869417bb6d142eaf6a109f30945d9d7ba
Peter MOUEZA <peter.poe11@formation.imie>
2017-02-09 14:12:33 +0100

    calculatrice function OK

--- angularServicesCalculatriceLim/script.js
+++ angularServicesCalculatriceLim/script.js
@@ -1,6 +1,6 @@
 var app = angular.module("app", []);
 
-app.controller("myController", function($scope){
+app.controller("myController",['$scope','addService','minusService', function($scope){
 // ça commence là
     $scope.showContent = true;
 
@@ -10,8 +10,18 @@ app.controller("myController", function($scope){
      	$scope.resultat = parseInt($scope.showContent1) + parseInt($scope.showContent2);
     }
 
+
+
+    $scope.minus = function(){
+     	$scope.resultat = parseInt($scope.showContent1) - parseInt($scope.showContent2);
+    }
        
 
 
+    $scope.addService = function(){
+     	$scope.resultat = parseInt($scope.showContent1) - parseInt($scope.showContent2);
+    }
+   }   
+]
 // ça se termine là
-});
+);
