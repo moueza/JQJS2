ea5fa00e278f6cc7ebb0c3460ed38637c307c148
peter <mouezapeter@gmail.com>
2017-02-10 08:00:05 +0100

    sov

--- angularServicesCalculatriceLim/script.js
+++ angularServicesCalculatriceLim/script.js
@@ -1,32 +1,41 @@
 var app = angular.module("app", []);
 
-app.controller("myController", function($scope,addService,minusService){
+app.controller("myController", function ($scope, addService, minusService) {
 // ça commence là
     $scope.showContent = true;
 
- 
 
-    $scope.add = function(){
-     	$scope.resultat = parseInt($scope.showContent1) + parseInt($scope.showContent2);
+    app.service('operation', function () {
+        this.ditHello = function (x) {
+            return "Hello";
+        }
+
+
+
+        this.addService = function () {
+           return  parseInt($scope.showContent1) - parseInt($scope.showContent2);
+        }
+
+
+        this.minusService = function () {
+            //$scope.resultat = parseInt($scope.showContent1) - parseInt($scope.showContent2);
+            return parseInt($scope.showContent1) - parseInt($scope.showContent2);
+        }
+    });
+
+    $scope.add = function () {
+        //$scope.resultat = parseInt($scope.showContent1) + parseInt($scope.showContent2);
+        $scope.resultat = operation.addService();
     }
 
 
 
-    $scope.minus = function(){
-     	$scope.resultat = parseInt($scope.showContent1) - parseInt($scope.showContent2);
-    }
-       
-
-
-    $scope.addService = function(){
-     	$scope.resultat = parseInt($scope.showContent1) - parseInt($scope.showContent2);
+    $scope.minus = function () {
+        $scope.resultat = parseInt($scope.showContent1) - parseInt($scope.showContent2);
     }
 
 
-    $scope.minusService = function(){
-     	$scope.resultat = parseInt($scope.showContent1) - parseInt($scope.showContent2);
-    }
-   }
+}
 
 // ça se termine là
 );
